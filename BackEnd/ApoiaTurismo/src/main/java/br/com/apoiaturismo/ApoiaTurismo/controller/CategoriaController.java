dce3c8255ab1afb258b7f2a1ce307badadf98b58
novaavos <avosjogos@gmail.com>
2021-11-04 16:35:28 -0300

    MODEL/REPOSITORY/CONTROLLER

---
+++ BackEnd/ApoiaTurismo/src/main/java/br/com/apoiaturismo/ApoiaTurismo/controller/CategoriaController.java
@@ -0,0 +1,49 @@
+package br.com.apoiaturismo.ApoiaTurismo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.apoiaturismo.ApoiaTurismo.model.Categoria;
+import br.com.apoiaturismo.ApoiaTurismo.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAllCategoria(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getByIDCategoria(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/categorias/{tipo}")
+	public ResponseEntity<List<Categoria>> getByTipoCategoria(@PathVariable String tipo){
+		return ResponseEntity.ok(repository.findAllByTipoContainingIgnoreCase(tipo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+	
+}
---
+++ BackEnd/ApoiaTurismo/src/test/java/br/com/apoiaturismo/ApoiaTurismo/ApoiaTurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apoiaturismo.ApoiaTurismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoiaTurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
