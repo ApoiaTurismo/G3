f50a16f5f53b0218320dc6e74745c4e8015341cb
Gustavo Avos <avosjogos@gmail.com>
2021-12-03 12:46:20 -0300

    projeto em angular e 5 componentes

--- FrontEnd/ApoiaTurismo/src/app/app.module.ts
+++ FrontEnd/ApoiaTurismo/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ContatoComponent } from './contato/contato.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { SobrenosComponent } from './sobrenos/sobrenos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    ContatoComponent,
+    CategoriasComponent,
+    SobrenosComponent
   ],
   imports: [
     BrowserModule,
